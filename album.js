16b4af077b081eba1bec4803486a3b6e6d5aa721
Andrew Grathwohl <andrew@grathwohl.me>
2020-06-24 12:24:29 -0400

    run stats on each submitted track as soon as new object is declared

--- album.js
+++ album.js
@@ -6,10 +6,13 @@ const Track = require('./track')
 class Album extends Pool {
   constructor(dir) {
     super(Track)
+    this.duration = 0.0
     this.dir = path.resolve(dir)
     this.sources = fs.readdirSync(this.dir).map(p => `${this.dir}/${p}`)
     for (const source of this.sources) {
-      this.add(new Track(source))
+      const newTrack = new Track(source)
+      newTrack.stats()
+      this.add(newTrack)
     }
   }
 }
