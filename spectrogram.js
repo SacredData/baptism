b8e8c8c6cf88c650046d286bf35ff080a7e82dd5
Andrew Grathwohl <andrew@grathwohl.me>
2020-06-27 19:21:59 -0400

    spectrogram functionality; flags JSON for storing SoX command line options

---
+++ spectrogram.js
@@ -0,0 +1,24 @@
+const debug = require('debug')('baptism:spectrogram')
+const path = require('path')
+const { spawn } = require('child_process')
+const spectrogramFlags = require('./flags.json').spectrogram.flags
+
+function main(file, cb) {
+  const uri = path.resolve(file)
+  const outFile = `${uri}.png`
+  const soxCmd = spawn('sox', [
+    uri,
+    '-n',
+    'spectrogram',
+    ...spectrogramFlags,
+    '-o',
+    outFile
+  ])
+  soxCmd.on('error', err => { throw new Error(err) })
+  soxCmd.on('close', (code) => {
+    debug('exitcode', code)
+    cb(null, outFile)
+  })
+}
+
+module.exports = main
