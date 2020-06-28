166842797d2acdf5a7d02cdd0b17745ee19560cc
Andrew Grathwohl <andrew@grathwohl.me>
2020-06-28 12:25:18 -0400

    fix Pool require statement; implement WIP validation method

--- album.js
+++ album.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
-const Pool = require('nanoresource-pool')
+const { Pool } = require('nanoresource-pool')
 const Track = require('./track')
 
 class Album extends Pool {
@@ -15,6 +15,12 @@ class Album extends Pool {
       newTrack.stats()
       newTrack.silence()
       this.add(newTrack)
+    }
+  }
+  validate() {
+    return {
+      dynamics: this.query().every(st => st.stats.peak.valid && st.stats.rms.valid),
+      silences: this.query().every(st => st.silences[1].start && st.silences[1].end)
     }
   }
 }
