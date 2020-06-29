8f34f10ce2408bd91e63ee737c30ae05114ea844
Andrew Grathwohl <andrew@grathwohl.me>
2020-06-29 18:56:13 -0400

    cleanup index; add spooky debug message

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Album = require('./album')
 const Track = require('./track')
-
 const flags = require('./flags.json')
-
 const soxi = require('./soxi')
 const stats = require('./stats')
 const spectrogram = require('./spectrogram')
+const debug = require('debug')('baptism:index')
+
+debug('Welcome to SACRED.AUDIO. This is SACRED1: Baptism.')
 
 module.exports = {
   Album, Track, flags, soxi, stats, spectrogram
