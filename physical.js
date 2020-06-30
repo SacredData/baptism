a4cad41454a2292b4a0fbd0e019fe50dd1208741
Andrew Grathwohl <andrew@grathwohl.me>
2020-06-30 17:37:32 -0400

    classes which represent physical and digital audio media

---
+++ physical.js
@@ -0,0 +1,46 @@
+const Album = require('./album')
+
+class Release {
+  constructor(opts={}) {
+    this.tracks = []
+    this.albums = []
+
+    if (opts.size) {
+      this.size = opts.size
+    }
+
+    if (opts.speed) {
+      this.speed = opts.speed
+    }
+  }
+  add(a) {
+    if (a instanceof Album) {
+      this.tracks.push(...a.query())
+      this.albums.push(a)
+    } else if (a instanceof Track) {
+      this.tracks.push(a)
+    }
+  }
+}
+
+class Digital extends Release {
+  constructor(opts={}) {
+    super(opts)
+  }
+}
+
+class Vinyl extends Release {
+  constructor(opts={}) {
+    super(opts)
+  }
+}
+
+class CD extends Release {
+  constructor() {
+    super()
+  }
+}
+
+module.exports = {
+  CD, Digital, Release, Vinyl
+}
