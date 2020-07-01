ac50cc903d7889546b7f2ff47e872e9f7c1088a2
Andrew Grathwohl <andrew@grathwohl.me>
2020-07-01 17:35:55 -0400

    more WIP modeling of Release classes

--- release.js
+++ release.js
@@ -5,29 +5,48 @@ class Release {
   constructor(opts={}) {
     this.tracks = []
     this.albums = []
+    this.duration = 0.0
 
     if (opts.size) {
-      this.size = opts.size
+      switch (opts.size) {
+        case 7:
+        case 10:
+        case 12:
+          this.size = opts.size
+          break
+        default:
+          throw new Error('Invalid size. Must be one of [7, 10, 12]')
+          break
+      }
     }
 
     if (opts.speed) {
-      this.speed = opts.speed
+      switch (opts.speed) {
+        case 33:
+        case 45:
+          this.speed = opts.speed
+          break
+        default:
+          throw new Error('Invalid speed. Must be one of [33, 45]')
+          break
+      }
     }
   }
   add(a) {
     if (a instanceof Album) {
       this.tracks.push(...a.query())
       this.albums.push(a)
+      this.duration += a.duration
     } else if (a instanceof Track) {
       this.tracks.push(a)
+      this.duration += a.duration
     }
   }
 }
 
 class CD extends Release {
   constructor(opts={}) {
-    super()
-    this.durationLimit = 4800
+    super(opts)
   }
 }
 
