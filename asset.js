4251e283c2779c3f7ed05b9eacc7ef42045963ba
Andrew Grathwohl <andrew@grathwohl.me>
2020-07-02 12:50:48 -0400

    introducing Asset class

---
+++ asset.js
@@ -0,0 +1,29 @@
+const debug = require('debug')('baptism:track')
+const fs = require('fs')
+const mime = require('mime')
+const Resource = require('nanoresource')
+
+class Asset extends Resource {
+  constructor(source, opts = {}) {
+    super()
+    this.filename = source
+    this.type = mime.getType(this.filename)
+    this.fd = 0
+  }
+
+  _open (cb) {
+    debug('Now opening file ...', this.filename)
+    fs.open(this.filename, 'r', (err, fd) => {
+      if (err) return cb(err)
+      this.fd = fd
+      cb(null)
+    })
+  }
+
+  _close (cb) {
+    debug('Now closing file ...')
+    fs.close(this.fd, cb)
+  }
+}
+
+module.exports = Asset
