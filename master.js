9a9d8a01583ea86ae765fb1facc317e7a1797bbc
agrathwohl <andrew@grathwohl.me>
2020-07-03 16:55:09 -0400

    master wip

--- master.js
+++ master.js
@@ -4,11 +4,19 @@ class Master extends Track {
   constructor(source, opts={}) {
     super(source, opts)
 
+    this.parent = null
+
     if (opts.parent) {
       if (opts.parent instanceof Track) {
         this.parent = opts.parent
+      } else {
+        throw new Error(`Invalid opts.parent: must be an instance of bap.Track`)
       }
     }
+  }
+
+  compare (cb) {
+    if (!this.parent) cb(new Error(`No parent, add one to the Master first.`))
   }
 }
 
