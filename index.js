e17a1f306568c2fdf8363d8f5208621eba8b12fe
Andrew Grathwohl <andrew@grathwohl.me>
2020-07-08 13:17:12 -0400

    export releases properly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Album = require('./album')
 const { Asset, CoverArt } = require('./asset')
 const { Master, Premaster } = require('./master')
-const Release = require('./release')
+const { CD, Digital, Download, Release, Stream, Vinyl } = require('./release')
 const Track = require('./track')
 const flags = require('./flags.json')
 const soxi = require('./soxi')
@@ -12,5 +12,6 @@ const debug = require('debug')('baptism:index')
 debug('Welcome to SACRED.AUDIO. This is SACRED1: Baptism.')
 
 module.exports = {
-  Album, Asset, CoverArt, Master, Premaster, Release, Track, flags, soxi, stats, spectrogram
+  Album, Asset, CD, CoverArt, Digital, Download, Master, Premaster, Release,
+  Stream, Track, Vinyl, flags, soxi, stats, spectrogram
 }
