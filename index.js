a3e7f646fa6964cfc3b6de116342cee88a40b1a2
Andrew Grathwohl <andrew@grathwohl.me>
2020-10-12 20:16:46 -0400

    Release 0.0.9

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { CD, Digital, Download, Release, Stream, Vinyl } = require('./release')
 const Track = require('./track')
 const fingerprint = require('./fingerprint')
 const flags = require('./flags.json')
+const peaks = require('./peaks')
 const soxi = require('./soxi')
 const stats = require('./stats')
 const spectrogram = require('./spectrogram')
@@ -14,5 +15,5 @@ debug('Welcome to SACRED.AUDIO. This is SACRED1: Baptism.')
 
 module.exports = {
   Album, Asset, CD, CoverArt, Digital, Download, Master, Premaster, Release,
-  Stream, Track, Vinyl, fingerprint, flags, soxi, stats, spectrogram
+  Stream, Track, Vinyl, fingerprint, flags, peaks, soxi, stats, spectrogram
 }
