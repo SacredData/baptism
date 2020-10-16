bebd4aaabcbd2097dda74ad116c4f8e3ce324e20
Andrew Grathwohl <gwohl@Andrews-MacBook-Pro.local>
2020-10-16 13:26:44 -0400

    cleanup

--- fingerprint.js
+++ fingerprint.js
@@ -2,7 +2,6 @@ const fpcalc = require('fpcalc')
 const path = require('path')
 
 function main(inFile, cb) {
-  // fpcalc(path.resolve(inFile), { raw: true }, function(err, result) {
   fpcalc(path.resolve(inFile), function(err, result) {
     if (err) { cb(err) }
     cb(null, result.fingerprint)
